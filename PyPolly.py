3ba58c49f16bdb3afd01ea088b38f65688be7f8b
femiimam001 <olufemi.imam@gmail.com>
2022-01-26 22:24:26 -0500

    Adding resources and project files

---
+++ PyPolly.py
@@ -0,0 +1,10 @@
+#The data we need to retive
+from random import vonmisesvariate
+
+
+# 1.The total number of vote cast.
+# 2.A complete list of all the candidates that recived vote
+# 3.The percentage of vote each candidate won.
+# 4.The total number of vote each candidate wion.
+# 5.The winner of the election based on popular vote.
+
